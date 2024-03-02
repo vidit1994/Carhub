b1802fafa8485ac32c68157251a0be984a4c4116
sakshi jain <sakshijain@sakshis-MacBook-Air.local>
2024-03-02 12:55:08 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
+import { fetchCars } from "@/utils";
+import { HomeProps } from "@/types";
+import { fuels, yearsOfProduction } from "@/constants";
+
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+
+      <div className="mt-12 padding-x padding-y max-with" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4x1 font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
+          </div>
+        </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+              <CarCard car={car} />
+             ))}
+            </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
+          </section>
+        ): (
+          <div className="home__error-container">
+            <h2 className="text-black text-x1 font-bold"
+            >Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+
+      </div>
+    </main>
+  );
+}
---
+++ components/index.ts
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
+
+export {
+    Hero,
+    CustomButton,
+    NavBar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    CarCard,
+    ShowMore
+}
